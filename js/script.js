721a5c5407884dd12df68c208043793f62a2e45c
objetive218 <davidsantiagotc@gmail.com>
2024-01-16 22:54:39 -0500

    logic update board

--- js/script.js
+++ js/script.js
@@ -9,62 +9,67 @@ const player = function (name, figure) {
   return { name, figure, points, givePoint, turn };
 };
 
-const gameBoard = function () {
+const gameBoard = (function () {
   let board = [
-    { position: a1, playerFigure: null },
-    { position: a2, playerFigure: null },
-    { position: a3, playerFigure: null },
-    { position: b1, playerFigure: null },
-    { position: b2, playerFigure: null },
-    { position: b3, playerFigure: null },
-    { position: c1, playerFigure: null },
-    { position: c2, playerFigure: null },
-    { position: c3, playerFigure: null },
+    { position: "a1", playerFigure: null },
+    { position: "a2", playerFigure: null },
+    { position: "a3", playerFigure: null },
+    { position: "b1", playerFigure: null },
+    { position: "b2", playerFigure: null },
+    { position: "b3", playerFigure: null },
+    { position: "c1", playerFigure: null },
+    { position: "c2", playerFigure: null },
+    { position: "c3", playerFigure: null },
   ];
 
-  let boardPosition = { a1, a2, a3, b1, b2, b3, c1, c2, c3 };
+  //let boardPosition = { a1, a2, a3, b1, b2, b3, c1, c2, c3 };
 
   const checkBoard = () => {
-    switch (board) {
-      case board[0].playerFigure === figure &&
-        board[1].playerFigure === figure &&
-        board[2].playerFigure === figure:
+    switch (true) {
+      case board[0].playerFigure === "x" &&
+        board[1].playerFigure === "x" &&
+        board[2].playerFigure === "x":
         console.log("win f.a");
         break;
-      case board[3].playerFigure === figure &&
-        board[4].playerFigure === figure &&
-        board[5].playerFigure === figure:
+      case board[3].playerFigure === "x" &&
+        board[4].playerFigure === "x" &&
+        board[5].playerFigure === "x":
         console.log("win f.b");
         break;
-      case board[6].playerFigure === figure &&
-        board[7].playerFigure === figure &&
-        board[8].playerFigure === figure:
+      case board[6].playerFigure === "x" &&
+        board[7].playerFigure === "x" &&
+        board[8].playerFigure === "x":
         console.log("win f.c");
         break;
-      case board[0].playerFigure === figure &&
-        board[3].playerFigure === figure &&
-        board[6].playerFigure === figure:
+      case board[0].playerFigure === "x" &&
+        board[3].playerFigure === "x" &&
+        board[6].playerFigure === "x":
         console.log("win c.board[0].position");
         break;
-      case board[1].playerFigure === figure &&
-        board[4].playerFigure === figure &&
-        board[7].playerFigure === figure:
+      case board[1].playerFigure === "x" &&
+        board[4].playerFigure === "x" &&
+        board[7].playerFigure === "x":
         console.log("win c.board[1].position");
         break;
-      case board[2].playerFigure === figure &&
-        board[5].playerFigure === figure &&
-        board[8].playerFigure === figure:
+      case board[2].playerFigure === "x" &&
+        board[5].playerFigure === "x" &&
+        board[8].playerFigure === "x":
         console.log("win c.board[2].position");
         break;
-      case board[0].playerFigure === figure &&
-        board[4].playerFigure === figure &&
-        board[8].playerFigure === figure:
+      case board[0].playerFigure === "x" &&
+        board[4].playerFigure === "x" &&
+        board[8].playerFigure === "x":
         console.log("win x.a");
         break;
-      case board[6].playerFigure === figure &&
-        b2 === figure &&
-        board[2].playerFigure === figure:
+      case board[6].playerFigure === "x" &&
+        b2 === "x" &&
+        board[2].playerFigure === "x":
         console.log("win x.c");
+        break;
+      case board.filter((e) => {
+        e.playerFigure === null;
+      }) === null:
+        console.log("it´s a tie");
         break;
       default:
         console.log("play continue");
@@ -72,9 +77,13 @@ const gameBoard = function () {
     }
   };
   return { checkBoard, board };
-};
+})();
 
 gameBoard.board[0].playerFigure = "x";
+gameBoard.checkBoard();
+gameBoard.board[1].playerFigure = "x";
+gameBoard.checkBoard();
+gameBoard.board[2].playerFigure = "x";
 gameBoard.checkBoard();
 
 //verificar que en el array no existan 3 figuras seguidad
