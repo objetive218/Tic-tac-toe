e5f0ac0769f0f10c48bd49030e2972405c204de2
objetive218 <davidsantiagotc@gmail.com>
2024-01-18 11:33:36 -0500

    add create game

--- js/script.js
+++ js/script.js
@@ -28,12 +28,12 @@ const gameBoard = (function () {
         return e.playerFigure === null;
       }) === false:
         scoreBoard.innerHTML = "it´s a tie";
-
+        gameClear();
         break;
       case board[0].playerFigure === player &&
         board[1].playerFigure === player &&
         board[2].playerFigure === player:
-        scoreBoard.innerHTML = `Win ${name}`;
+        scoreBoard.innerHTML = `Win ${name}!`;
         winner = true;
         gameClear();
         console.log(winner);
@@ -41,37 +41,44 @@ const gameBoard = (function () {
       case board[3].playerFigure === player &&
         board[4].playerFigure === player &&
         board[5].playerFigure === player:
-        scoreBoard.innerHTML = `Win ${name}`;
+        scoreBoard.innerHTML = `Win ${name}!`;
+        gameClear();
         break;
       case board[6].playerFigure === player &&
         board[7].playerFigure === player &&
         board[8].playerFigure === player:
-        scoreBoard.innerHTML = `Win ${name}`;
+        scoreBoard.innerHTML = `Win ${name}!`;
+        gameClear();
         break;
       case board[0].playerFigure === player &&
         board[3].playerFigure === player &&
         board[6].playerFigure === player:
-        scoreBoard.innerHTML = `Win ${name}`;
+        scoreBoard.innerHTML = `Win ${name}!`;
+        gameClear();
         break;
       case board[1].playerFigure === player &&
         board[4].playerFigure === player &&
         board[7].playerFigure === player:
-        scoreBoard.innerHTML = `Win ${name}`;
+        scoreBoard.innerHTML = `Win ${name}!`;
+        gameClear();
         break;
       case board[2].playerFigure === player &&
         board[5].playerFigure === player &&
         board[8].playerFigure === player:
-        scoreBoard.innerHTML = `Win ${name}`;
+        scoreBoard.innerHTML = `Win ${name}!`;
+        gameClear();
         break;
       case board[0].playerFigure === player &&
         board[4].playerFigure === player &&
         board[8].playerFigure === player:
-        scoreBoard.innerHTML = `Win ${name}`;
+        scoreBoard.innerHTML = `Win ${name}!`;
+        gameClear();
         break;
       case board[6].playerFigure === player &&
         board[4].playerFigure === player &&
         board[2].playerFigure === player:
-        scoreBoard.innerHTML = `Win ${name}`;
+        scoreBoard.innerHTML = `Win ${name}!`;
+        gameClear();
         break;
       default:
         console.log("play continue");
@@ -89,26 +96,37 @@ const play = (function () {
 
   function game(e) {
     e.preventDefault();
+    let pjTurn = player1.turn ? player2 : player1;
     let playerTurn = player1.turn ? player1 : player2;
-    scoreBoard.innerHTML = `${playerTurn.name}´s turn`;
+    scoreBoard.innerHTML = `${pjTurn.name}´s turn`;
     board[e.target.value].playerFigure = playerTurn.figure;
     checkBoard(playerTurn.figure, playerTurn.name);
     console.log(board);
     player1.turn ? (player1.turn = false) : (player1.turn = true);
     player2.turn === false ? (player2.turn = true) : (player2.turn = false);
   }
+  function createGame(){
+    main.innerHTML += `<div id="box">
+            <button id="zero" value="0" ></button>
+            <button id="one" value="1"></button>
+            <button id="two" value="2"></button>
+            <button id="three" value="3"></button>
+            <button id="four" value="4"></button>
+            <button id="five" value="5"></button>
+            <button id="six" value="6"></button>
+            <button id="sevent" value="7"></button>
+            <button id="eight" value="8"></button>
+        </div>`; 
+  }
 
-  return { createPlayer, game };
+
+  return { createPlayer, game, createGame };
 })();
 
 const player1 = play.createPlayer("Player 1", "x", true);
 const player2 = play.createPlayer("Player 2", "o", false);
 console.log(player1.winner);
 console.log(player1.winner);
-//verificar que en el array no existan 3 figuras seguidad
-// switch que tenga los caso de victoria y solo cambie la figura en la veificacion (8 casos) check
-//
-//assign events to divs
 
 let buttonList = box.getElementsByTagName("button");
 for (let index = 0; index < buttonList.length; index++) {
@@ -116,13 +134,15 @@ for (let index = 0; index < buttonList.length; index++) {
   let getId = actButton.id;
   actButton.addEventListener("click", (e) => {
     e.preventDefault();
-    if (gameBoard.winner) {
-      document.querySelector(`#${getId}`).disabled = true;
-    } else {
-      play.game(e);
-      document.querySelector(`#${getId}`).innerHTML = player1.turn
-        ? player2.figure
-        : player1.figure;
-    }
+    play.game(e);
+    document.querySelector(`#${getId}`).innerHTML = player1.turn
+      ? player2.figure
+      : player1.figure;
   });
 }
+
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+  box.innerHTML = "";
+
+})
